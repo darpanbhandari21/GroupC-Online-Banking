14a32a6b53ad5e0c3dfd1d5728ce3bd153600c85
Milan Sapkota <mylan310@gmail.com>
2021-03-29 23:33:51 -0500

    userController mockup

--- src/main/java/com/groupcbanking/uis/controller/UserController.java
+++ src/main/java/com/groupcbanking/uis/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.groupcbanking.uis.controller;
 
-public class UserController {
+import javax.swing.*;
+
+public class UserController
+{
+    public static void main(String[] args)
+    {
+        String decision;
+        do{
+            String operation = JOptionPane.showInputDialog("Enter operation: create|deposit|withdraw|balance");
+            switch(operation)
+            {
+                case "create":
+                    break;
+                case "deposit":
+                    break;
+                case "withdraw":
+                    break;
+                case "balance":
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Wrong Selection!!!");
+            }
+            decision = JOptionPane.showInputDialog("Do you want to continue? Enter y|n");
+        }while(decision.equalsIgnoreCase("y"));
+        JOptionPane.showMessageDialog(null, "Good Day.");
+    }
 }
