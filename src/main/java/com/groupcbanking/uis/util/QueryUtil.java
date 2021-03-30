0531ff1aa6934d3b0c707ff06d8fbbd47d88e5bd
Milan Sapkota <mylan310@gmail.com>
2021-03-29 23:40:41 -0500

    need to figure out the two tables

--- src/main/java/com/groupcbanking/uis/util/QueryUtil.java
+++ src/main/java/com/groupcbanking/uis/util/QueryUtil.java
@@ -2,9 +2,9 @@ package com.groupcbanking.uis.util;
 
 public class QueryUtil {
 
-    public static final String SAVE_SQL = "insert into user_tbl(user_name, password, mobile_no, salary, dob, active)values(?,?,?,?,?,?)";
-    public static final String UPDATE_SQL = "update user_tbl set user_name=?, password=?, mobile_no=?, salary=?, dob=?, active=? where id = ?";
-    public static final String DELETE_SQL = "delete from user_tbl where id = ?";
-    public static final String LIST_SQL = "select * from user_tbl";
-    public static final String GET_BY_ID_SQL = "select * from user_tbl where id = ?";
+    public static final String SAVE_SQL = "insert into account_tbl(account_name, account_no, id, email,mobile_no)values(?,?,?,?,?)";
+//    public static final String UPDATE_SQL = "update account_tbl set user_name=?, password=?, mobile_no=?, salary=?, dob=?, active=? where id = ?";
+//    public static final String DELETE_SQL = "delete from user_tbl where id = ?";
+//    public static final String LIST_SQL = "select * from user_tbl";
+//    public static final String GET_BY_ID_SQL = "select * from user_tbl where id = ?";
 }
